9cd5c9de0c23578c8fc59166a959fad7e93c4679
Raees Haider <149859364+RaeesHaider572@users.noreply.github.com>
2026-05-20 17:55:44 +0500

    employee icon solved

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -15,7 +15,7 @@ import {
   Warehouse,
   Percent as PercentIcon,
   AccountBalance as AccountBalanceIcon,
-  employee as EmployeeIcon,
+  VerifiedUser as EmployeeIcon,
 } from '@mui/icons-material';
 import { useThemeContext } from '../../contexts/ThemeContext';
 import { useAuth } from '../../contexts/AuthContext';
